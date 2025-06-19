1147d3289448c0c56efa13d1bd4c4aca45699458
Austin Gamez <austingamez91@gmail.com>
2025-06-19 17:06:50 +0100

    clean

--- api/plugins/lifespan.ts
+++ api/plugins/lifespan.ts
@@ -1,24 +1,39 @@
 import { FastifyInstance } from "fastify";
 import { dataExists } from "../../common/db";
-import { logger } from "../../common/logger";
 import fp from "fastify-plugin";
 
+// set up all lifecycle activities.
+// and handle shutdowns.
+
 async function lifespanPlugin(app: FastifyInstance) {
+  app.addHook("onClose", async () => {
+    app.log.info("Fastify shutting down gracefully.");
+  });
+  // hopefully this works
+  ["SIGTERM", "SIGINT"].forEach((signal) => {
+    process.on(signal, async () => {
+      app.log.info(`☠️ Caught ${signal}. Goodbye cruel cloud~`);
+      await app.close();
+      process.exit(0);
+    });
+  });
+
+
   const maxRetries = 30;
   const delay = 2000;
 
   for (let attempt = 0; attempt < maxRetries; attempt++) {
     const ready = await dataExists();
     if (ready) {
-      logger.info("The data exists!");
+      app.log.info("The data exists!");
       return;
     }
-    logger.info(`Waiting for data... attempt ${attempt + 1}/${maxRetries}`);
+    app.log.info(`Waiting for data... attempt ${attempt + 1}/${maxRetries}`);
     await new Promise((res) => setTimeout(res, delay));
   }
 
-  logger.error("Data did not become available in time. Failing startup.");
-  throw new Error("Startup aborted due to missing data");
+  app.log.error("Data did not become available in time. Failing startup.");
+  await app.close()
 }
 
 
