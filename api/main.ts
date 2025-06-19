66f8baee813ffdf99a9b0c5b63fe6c57b6745cde
Austin Gamez <austingamez91@gmail.com>
2025-06-19 15:04:33 +0100

    datadog-debug

--- api/main.ts
+++ api/main.ts
@@ -1,11 +1,18 @@
 import tracer from 'dd-trace';
-tracer.init();
+tracer.init({
+  logInjection: true,
+  service: 'cars-api',
+  env: 'production',
+});
+console.log('DD_SITE:', process.env.DD_SITE);
+console.log('DD_TRACE_AGENT_URL:', process.env.DD_TRACE_AGENT_URL);
+console.log('DD_API_KEY:', process.env.DD_API_KEY);
 
 import Fastify from "fastify";
 import { registerRoutes } from "./router/endpoints";
 import lifespanPlugin from "./plugins/lifespan";
-import { loggerOptions } from "../common/logger";
-
+import { logger, loggerOptions } from "../common/logger";
+logger.info("dd-trace enabled. Starting service.")
 const app = Fastify({ logger: loggerOptions, ignoreTrailingSlash: true, pluginTimeout:0 });
 const port = process.env.port ? parseInt(process.env.PORT) : 3000;
 
